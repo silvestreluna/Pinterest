f534eb6dbfa9a70ae4c2040e572c3e06d08e8713
Silvestre Luna <slvtrluna@gmail.com>
2019-05-14 20:52:23 -0500

    prining pins to dom

--- src/javascripts/components/pins/pins.js
+++ src/javascripts/components/pins/pins.js
@@ -1,9 +1,19 @@
 import pinsData from '../../helpers/data/pinsData';
+import util from '../../helpers/util';
+
+const writePins = (pins) => {
+  let domString = '';
+  pins.forEach((pin) => {
+    domString += `<img src="${pin.imageUrl}" alt="pin image">`;
+  });
+  util.printToDom('pins-on-board', domString);
+};
 
 const initPins = (boardId) => {
   pinsData.loadPinsForBoard(boardId)
     .then((pins) => {
       console.error('All pins', pins);
+      writePins(pins);
     })
     .catch(err => console.error(err));
 };
