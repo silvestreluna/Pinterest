1f56a6ec619bdb318d328950ed354e97cb171061
Silvestre Luna <slvtrluna@gmail.com>
2019-05-14 20:58:11 -0500

    completd pins

--- src/javascripts/components/pins/pins.js
+++ src/javascripts/components/pins/pins.js
@@ -1,6 +1,13 @@
 import pinsData from '../../helpers/data/pinsData';
 import util from '../../helpers/util';
 
+const bindEvents = () => {
+  document.getElementById('toBoardsBtn').addEventListener('click', () => {
+    document.getElementById('boards-page').classList.remove('hide');
+    document.getElementById('pins-page').classList.add('hide');
+  });
+};
+
 const writePins = (pins) => {
   let domString = '';
   pins.forEach((pin) => {
@@ -10,6 +17,7 @@ const writePins = (pins) => {
 };
 
 const initPins = (boardId) => {
+  bindEvents();
   pinsData.loadPinsForBoard(boardId)
     .then((pins) => {
       console.error('All pins', pins);
